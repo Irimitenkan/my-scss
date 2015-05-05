a1776880fc51cdf060a831863d556770858ecf03
Irimitenkan <RohacekD@gmail.com>
2015-05-05 21:51:05 +0200

    Complete setUp stack in grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,27 @@ module.exports = function(grunt) {
             build: {
                 src: 'src/script.js',
                 dest: 'js/script.min.js'
+            },
+            libUglify: {
+                files: {
+                    'js/lib/jquery.min.js': ['lib/jquery/jquery.js'],
+                    'js/lib/bootstrap.min.js': ['lib/sass-bootstrap/bootstrap.js']
+
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            libUglify: {
+                files: {
+                    'css/bootstrap.min.css': ['lib/sass-bootstrap/bootstrap.css']
+                }
+            },
+            build: {
+
             }
         },
         bower: {
@@ -21,22 +42,24 @@ module.exports = function(grunt) {
             }
         },
         copy: {
-            main: {
+            setUp: {
                 files: [
-                    {expand: true, cwd: 'lib/jquery/' ,src: ['**/jquery.js'], dest: 'js/lib', filter: 'isFile'},
-                    {expand: true, cwd: 'lib/sass-bootstrap/' ,src: ['**/*.js'], dest: 'js/lib', filter: 'isFile'},
-                    {expand: true, cwd: 'lib/sass-bootstrap/' ,src: ['**/*.css'], dest: 'css/', filter: 'isFile'},
                     {expand: true, cwd: 'lib/sass-bootstrap/' ,src: ['**/glyphicons*.*'], dest: 'fonts/', filter: 'isFile'},
+                    {expand: true, cwd: 'lib/matchHeight/' ,src: ['**/jquery.matchHeight-min.js'], dest: 'js/lib/', filter: 'isFile'},
                 ]
             }
         }
     });
 
+
     // Load the plugin that provides the "uglify" task.
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-bower-task');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
     // Default task(s).
     grunt.registerTask('default', ['uglify']);
+    // Set Up task
+    grunt.registerTask('setUp', ['bower:install', 'uglify:libUglify', 'cssmin:libUglify', 'copy:setUp']);
 };
